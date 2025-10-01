f3421148a03801eed2b2fb2d48c7c05834e01cf7
RichoAlbertTio <study160702@gmail.com>
2025-10-02 00:27:48 +0700

    update config db

--- internal/database/database.go
+++ internal/database/database.go
@@ -12,35 +12,20 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	// Baca konfigurasi database dari environment variables
-	dbHost := os.Getenv("DB_HOST")
-	if dbHost == "" {
-		dbHost = "localhost"
+	// Cek apakah DATABASE_URL tersedia
+	databaseURL := os.Getenv("DATABASE_URL")
+	var dsn string
+	if databaseURL != "" {
+		dsn = databaseURL
+	} else {
+		// Gunakan variabel environment Railway/postgres
+		dbHost := os.Getenv("PGHOST")
+		dbPort := os.Getenv("PGPORT")
+		dbUser := os.Getenv("PGUSER")
+		dbPassword := os.Getenv("PGPASSWORD")
+		dbName := os.Getenv("PGDATABASE")
+		dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
 	}
-	
-	dbPort := os.Getenv("DB_PORT")
-	if dbPort == "" {
-		dbPort = "5432"
-	}
-	
-	dbUser := os.Getenv("DB_USER")
-	if dbUser == "" {
-		dbUser = "postgres"
-	}
-	
-	dbPassword := os.Getenv("DB_PASSWORD")
-	if dbPassword == "" {
-		dbPassword = "richo123"
-	}
-	
-	dbName := os.Getenv("DB_NAME")
-	if dbName == "" {
-		dbName = "db_quiz13"
-	}
-
-	// Format DSN untuk PostgreSQL
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		dbHost, dbPort, dbUser, dbPassword, dbName)
 
 	var err error
 	DB, err = sql.Open("postgres", dsn)
